f9bd99a42a8d11f9fd8d5dd4956db85ac471af42
Rory Wagstaff <rfwagstaff@gmail.com>
2019-10-22 16:32:07 +0100

    Added BoardHelper Class to find available columns

---
+++ QuadRelate.Helpers/BoardHelper.cs
@@ -0,0 +1,23 @@
+﻿using QuadRelate.Types;
+using System.Collections.Generic;
+
+namespace QuadRelate.Helpers
+{
+    public class BoardHelper
+    {
+        public List<int> AvailableColumns(Board board)
+        {
+            var availableColumns = new List<int>();
+
+            for (var x = 0; x < Board.Width; x++)
+            {
+                if (board[x, Board.Height - 1] == Cell.Empty)
+                {
+                    availableColumns.Add(x);
+                }
+            }
+
+            return availableColumns;
+        }
+    }
+}
---
+++ QuadRelate.Tests/BoardHelperTests.cs
@@ -0,0 +1,84 @@
+﻿using Moq;
+using QuadRelate.Helpers;
+using QuadRelate.Types;
+using System.Collections.Generic;
+using Xunit;
+
+namespace QuadRelate.Tests
+{
+    public class BoardHelperTests
+    {
+        private readonly BoardHelper _boardHelper;
+        private readonly Board _board;
+
+        public BoardHelperTests()
+        {
+            _boardHelper = new BoardHelper();
+            _board = new Board();
+        }
+
+        [Fact]
+        public void AvailableColumns_ForFullBoard_ReturnsEmptyList()
+        {
+            // Fill board in with red counters
+            for (var x = 0; x < Board.Width; x++)
+            {
+                for (var y = 0; y < Board.Height; y++)
+                {
+                    _board[x, y] = Cell.Red;
+                }
+            }
+
+            var actual = _boardHelper.AvailableColumns(_board);
+
+            Assert.Equal(new List<int>(), actual);
+        }
+
+        [Fact]
+        public void AvailableColumns_ForIncompleteBoard_ReturnsFullList()
+        {
+            var expected = new List<int>();
+
+            // Expected list of all columns
+            for (var i = 0; i < Board.Width; i++)
+            {
+                expected.Add(i);
+            }
+
+            // Fill board in with red counters
+            for (var x = 0; x < Board.Width; x++)
+            {
+                for (var y = 0; y < Board.Height - 1; y++)
+                {
+                    _board[x, y] = Cell.Red;
+                }
+            }
+
+            var actual = _boardHelper.AvailableColumns(_board);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AvailableColumns_ForPartlyFullBoard_ReturnsFullList()
+        {
+            var expected = new List<int>();
+
+            // Expected list of final column only
+            expected.Add(Board.Width - 1);
+
+            // Fill board in with red counters
+            for (var x = 0; x < Board.Width - 1; x++)
+            {
+                for (var y = 0; y < Board.Height; y++)
+                {
+                    _board[x, y] = Cell.Red;
+                }
+            }
+
+            var actual = _boardHelper.AvailableColumns(_board);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
