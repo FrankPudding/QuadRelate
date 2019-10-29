2e1b386c7ddaa380116a53f2426ac6f49bfe56b7
Rory Wagstaff <rfwagstaff@gmail.com>
2019-10-29 16:39:40 +0000

    Added CPUPlayerRandom

---
+++ QuadRelate.Models/CPUPlayerRandom.cs
@@ -0,0 +1,19 @@
+﻿using QuadRelate.Contracts;
+using QuadRelate.Helpers;
+using QuadRelate.Types;
+using System;
+
+namespace QuadRelate.Models
+{
+    public class CPUPlayerRandom : ICPUPlayer
+    {
+        public int NextMove(Board board)
+        {
+            var random = new Random();
+
+            var index = random.Next(board.AvailableColumns().Count);
+
+            return board.AvailableColumns()[index];
+        }
+    }
+}
