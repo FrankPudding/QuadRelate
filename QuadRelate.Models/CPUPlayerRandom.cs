745978c24d43278e259458a1be1b47c823021839
Rory Wagstaff <rfwagstaff@gmail.com>
2019-10-29 19:40:32 +0000

    Moved Random to constructor form NextMove method

--- QuadRelate.Models/CPUPlayerRandom.cs
+++ QuadRelate.Models/CPUPlayerRandom.cs
@@ -7,11 +7,16 @@ namespace QuadRelate.Models
 {
     public class CPUPlayerRandom : ICPUPlayer
     {
+        private Random _randomNumber;
+
+        public CPUPlayerRandom()
+        {
+            _randomNumber = new Random();
+        }
+
         public int NextMove(Board board)
         {
-            var random = new Random();
-
-            var index = random.Next(board.AvailableColumns().Count);
+            var index = _randomNumber.Next(board.AvailableColumns().Count);
 
             return board.AvailableColumns()[index];
         }
