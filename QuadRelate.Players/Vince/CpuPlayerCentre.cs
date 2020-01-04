72c1a3a5ac592ec3f9548e992f8de15eba0fd881
Vince Melia <vince@drifta.co.uk>
2020-01-04 11:50:19 +0000

    Added new simple CPU player.

---
+++ QuadRelate.Players/Vince/CpuPlayerCentre.cs
@@ -0,0 +1,28 @@
+﻿using QuadRelate.Contracts;
+using QuadRelate.Models;
+using QuadRelate.Types;
+
+namespace QuadRelate.Players.Vince
+{
+    public class CpuPlayerCentre : IPlayer
+    {
+        private readonly IRandomizer _randomizer;
+
+        public CpuPlayerCentre(IRandomizer randomizer)
+        {
+            _randomizer = randomizer;
+        }
+
+        public string Name => "The Centaur";
+
+        public int NextMove(Board board, Counter colour)
+        {
+            var centreMoves = MovesHelper.GetMovesClosestToCentre(board.AvailableColumns());
+            return centreMoves[_randomizer.Next(centreMoves.Count)];
+        }
+
+        public void GameOver(GameResult result)
+        {
+        }
+    }
+}
